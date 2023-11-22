b3883b7d4ea96a040be189f24295c04bdca2dcf7
rajpatel2435 <rajpatel2435@gmail.com>
2023-11-21 22:40:52 -0500

    Fix: CSS and work Method

---
+++ pages/components/WorkMethod.tsx
@@ -0,0 +1,189 @@
+import React from 'react'
+
+function WorkMethod() {
+    return (
+        <>
+            <div className=' block h-auto w-auto bg-black text-white'>
+
+            <h2 className=' text-4xl uppercase bg-white text-black text-center  '>Our Approach</h2>
+
+                {/* 
+               
+
+{/* 
+                <h2 className=' text-4xl uppercase  ml-28  '>Our Approach</h2> */}
+           
+           <div className='grid grid-cols-1 gap-11 py-5 px-10 md:grid-cols-3'>
+
+<div className=' ml-auto mr-auto md:w-96'>
+    <ul >
+        <li className='text-2xl font-bold  '>1. Discovery Phase</li>
+        <li className='text-xl'>- Delve into business goals and audience dynamics</li>
+        <li className='text-xl'>- Identify challenges and unveil opportunities
+        </li>
+
+    </ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+    <ul >
+        <li className='text-2xl font-bold  '>2. Research and Analysis</li>
+        <li className='text-xl'>- Navigate through market intricacies</li>
+        <li className='text-xl'>- Illuminate strategic pathways
+        </li>
+
+    </ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+    <ul >
+        <li className='text-2xl font-bold  '>3. Planning and Strategy</li>
+        <li className='text-xl'>- Architect a detailed project blueprint</li>
+        <li className='text-xl'>- Define milestones, set timelines, and chart KPIs
+        </li>
+
+    </ul>
+</div>
+
+
+
+</div>
+
+<h2 className='ml-28 text-4xl uppercase  '>Design Process</h2>
+
+<div className='grid grid-cols-1 gap-11 py-5 px-10 md:grid-cols-3 '>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>1.Wireframing</li>
+<li className='text-xl'>- Craft visual blueprints for aesthetic precision</li>
+<li className='text-xl'>- Establish the project's skeletal elegance
+</li>
+
+</ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>2.  Prototyping</li>
+<li className='text-xl'>- Sculpt interactive prototypes for tangible vision</li>
+<li className='text-xl'>- Carve refinement paths based on insightful feedback
+</li>
+
+</ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>3. User Experience (UX) Design</li>
+<li className='text-xl'>- Weave intuitive user flows</li>
+<li className='text-xl'>- Craft an engaging digital tapestry
+</li>
+
+</ul>
+</div>
+
+
+
+</div>
+
+<h2 className='ml-28 text-4xl uppercase  '> DEvelopment Approach</h2>
+
+
+<div className='grid grid-cols-1 gap-11 py-5 px-10 md:grid-cols-3 '>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>1.Coding Standards</li>
+<li className='text-xl'>- Adhere to industry commandments</li>
+<li className='text-xl'>-Forge code: robust, reliable, and refined
+</li>
+
+</ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>2.  Iterative Development</li>
+<li className='text-xl'>- Dance with iteration, refine in rhythm</li>
+<li className='text-xl'>-Mold excellence with the sculptor's chisel of feedback
+</li>
+
+</ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>3. Testing Procedures</li>
+<li className='text-xl'>- Engage in rigorous functional and security theatrics</li>
+<li className='text-xl'>- Spot and swiftly vanquish lurking issues
+</li>
+
+</ul>
+</div>
+
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>4.Deployment and Launch</li>
+<li className='text-xl'>- Choreograph a secure and seamless debut</li>
+<li className='text-xl'>- Stand vigilant, offering a post-launch curtain call
+</li>
+
+</ul>
+</div>
+
+
+
+</div>
+
+
+
+<h2> Communication</h2>
+
+
+<div className='grid grid-cols-1 gap-11 py-5 px-10 md:grid-cols-3 '>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>1. Transparent Collaboration:</li>
+<li className='text-xl'>- Weave a tapestry of regular updates and progress reports</li>
+<li className='text-xl'>-Utilize collaborative tools for a symphony of real-time feedback
+</li>
+
+</ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>2.   Client Training::</li>
+<li className='text-xl'>- Conduct training serenades for mastery</li>
+<li className='text-xl'>-Empower clients with the keys to long-term success
+</li>
+
+</ul>
+</div>
+
+<div className=' ml-auto mr-auto md:w-96'>
+<ul >
+<li className='text-2xl font-bold  '>3. Testing Procedures:</li>
+<li className='text-xl'>- Engage in rigorous functional and security theatrics</li>
+<li className='text-xl'>- Spot and swiftly vanquish lurking issues
+</li>
+
+</ul>
+</div>
+
+
+
+</div>
+
+               
+            </div>
+        </>
+    )
+}
+
+export default WorkMethod
+
+
