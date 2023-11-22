3ae2a2788f6bea7f9f3bd9e22c2bc28c5d378137
rajpatel2435 <rajpatel2435@gmail.com>
2023-11-21 22:40:34 -0500

    feat: project list feture

--- pages/components/ProjectLi.tsx
+++ pages/components/ProjectLi.tsx
@@ -1,126 +1,349 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+// import gsap from "gsap";
+// import { Power4 } from "gsap";
 
+// function ProjetcLi() {
+//   useEffect(() => {
+//     const liItems = document.querySelectorAll(".custom-li");
+
+//     liItems.forEach((li, index) => {
+//       const liInside = li.querySelector(".li_inside");
+
+//       li.addEventListener("mouseover", () => {
+//         const direction = index % 2 === 0 ? 1 : -1; // 1 for left to right, -1 for right to left
+
+//         gsap.to(liInside, {
+//           duration: 1,
+//           ease: Power4.easeOut,
+//           css: {
+//             width: "100%",
+//             background: "white",
+//           },
+//         });
+
+//         gsap.to(liInside, {
+//           duration: 1,
+//           scaleX: direction,
+//         });
+//       });
+
+//       li.addEventListener("mouseout", () => {
+//         gsap.to(liInside, {
+//           duration: 0.9,
+//           css: {
+//             width: "0%",
+//             background: "black",
+//           },
+//         });
+
+//         gsap.to(liInside, {
+//           duration: 1,
+//           scaleX: 1,
+//         });
+//       });
+//     });
+//   }, []);
+
+//   return (
+//     <>
+//       <div className="bg-black text-white">
+//         <ul className="li_container">
+//           <li className="custom-li">
+//             <div className="li_inside">
+//               <h1 className="text-green-700 w-96  h1_raj">Awesome Text</h1>
+//             </div>
+//           </li>
+//           <li className="custom-li">
+//             <div className="li_inside">
+//               <h1 className="text-green-700 w-96 h1_raj">Awesome Text</h1>
+//             </div>
+//           </li>
+
+//           <li className="custom-li">
+//             <div className="li_inside">
+//               <h1 className="text-green-700 w-96 h1_raj">Awesome Text</h1>
+//             </div>
+//           </li>
+
+//           <li className="custom-li">
+//             <div className="li_inside">
+//               <h1 className="text-green-700 w-96 h1_raj">Awesome Text</h1>
+//             </div>
+//           </li>
+//           <li className="custom-li">
+//             <div className="li_inside">
+//               <h1 className="text-green-700 w-96 h1_raj">Awesome Text</h1>
+//             </div>
+//           </li>
+//           {/* Add more li elements as needed */}
+//         </ul>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default ProjetcLi;
+
+
+
+
+
+import React, { useEffect } from "react";
 import gsap from "gsap";
 import { Power4 } from "gsap";
+import { Expo } from "gsap";
+import Link from "next/link";
 
 function ProjetcLi() {
   useEffect(() => {
-    const li_1 = document.querySelector(".ul");
-    const li_inside = document.querySelector(".li_raj");
-    const raj = document.querySelector(".h1_raj");
-    const li__inside = document.querySelector(".li_inside");
-    const randomValue = Math.floor(Math.random() * 100) + 1;
+    const liItems = document.querySelectorAll(".custom-li");
 
-    li_1?.addEventListener("mouseover", () => {
-      gsap.to(li__inside, {
-        duration: 1,
-        ease: Power4.easeOut,
-        css: {
-          width: "100%",
-          background: "white",
-          color: "black",
-        },
+    liItems.forEach((li, index) => {
+      const liInside = li.querySelector(".li_inside");
+      const liInside1 = li.querySelector(".rajjj");
+
+      li.addEventListener("mouseover", () => {
+        const direction = index % 2 === 0 ? 1 : -1; // 1 for left to right, -1 for right to left
+
+
+        
+        gsap.to(liInside1, {
+          color:"black"
+        });
+
+        gsap.to(liInside, {
+          duration: 1,
+          ease: Power4.easeOut,
+          css: {
+            width: "100%",
+            background: "white",
+          },
+          onComplete: () => {
+            gsap.set(liInside, { clearProps: "all" });
+          },
+        });
+
+        gsap.to(liInside, {
+          duration: 1,
+          scaleX: direction,
+        });
+
       });
 
-      gsap.to(raj, {
-        duration: 1,
-        ease: Power4.easeIn,
-        css: {
-          color: "black",
-        },
+      li.addEventListener("mouseout", () => {
+        gsap.to(liInside, {
+          duration: 0.9,
+          css: {
+            width: "0%",
+          },
+          onComplete: () => {
+            gsap.set(liInside, { clearProps: "all" });
+          },
+        });
+
+        gsap.to(liInside, {
+          duration: 1,
+          scaleX: 1,
+        });
       });
     });
+  }, []);
 
-    //     li_inside?.addEventListener("mouseover", () => {
-    //       gsap.to(li_inside, {
-    //         duration: 0.8,
-    //         css: {
-    //           flex: "1",
-    //           background: "green",
-    //         },
-    //       });
 
-    //       gsap.to(raj,{
-    //         duration:0.8,
-    //         css:{
-    //           width:"100%",
-    //           background: "white",
-    //           color:"black",
-    //           fontSize:"50px"
-    //         }
-    //       })
 
-    //     });
+  useEffect(() => {
+    let imgHoverLink = gsap.utils.toArray(".img-hover-effect-link");
+    let imgWrap = document.querySelector('.img-wrapper');
+    let imgItem = document.querySelector('.img-placeholder img');
 
-    li_1?.addEventListener("mouseout", () => {
-      gsap.to(li__inside, {
-        duration: 0.9,
-
-        css: {
-          width: "0%",
-    
-        },
-      });
-
-      gsap.to(raj, {
+    function moveImg(e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+      let tl = gsap.timeline();
+      tl.to(imgWrap, {
         duration: 1,
-        css: {
-          color: "green",
-        },
+        x: mouseX,
+        y: mouseY,
+        ease: Expo.ease,
       });
-      //       gsap.to(li_inside, {
-      //         duration: 0.8,
-      //         css: {
+    }
 
-      // flex:"0.3"
-      //         },
-      //       });
+    function linkHover(e) {
+      if (e.type === "mouseenter") {
+        let imgSrc = e.target.dataset.src;
+        let tl = gsap.timeline();
 
-      //       gsap.to(li_inside, {
-      //         delay: 0.9,
-      //         duration:0.8,
-      //         css: {
+        tl.set(imgItem, {
+          attr: { src: imgSrc } // update imgSrc as in data-src
+        }).to(imgWrap, {
+          autoAlpha: 1,
+          scale: 1,
+          zIndex:10000,
+        });
+      }
 
-      // background:"unset",
-      //         },
-      //       });
+      if (e.type === "mouseleave") {
+        let tl = gsap.timeline();
+        tl.to(imgWrap, {
+          autoAlpha: 0,
+          scale: 0.3,
+        });
+      }
+    }
 
-      // gsap.fromTo(li_1, { backgroundSize: "100% 100%" },{ backgroundSize: `100% 0%`, duration: 0.8})
-    });
-  });
+    function initAnimation() {
+      imgHoverLink.forEach((link) => {
+        link.addEventListener('mouseenter', linkHover);
+        link.addEventListener('mouseleave', linkHover);
+        link.addEventListener('mousemove', moveImg);
+      });
+    }
+
+    function init() {
+      initAnimation();
+    }
+
+    init();
+  }, []); // Empty dependency array ensures useEffect runs only on mount
+
   return (
     <>
-    <div className="bg-black text-white">
-      <ul className="li_container">
-        {/* <li className='li_1 '>
-        <div className=' flex justify-start  align-middle h-36 bg-slate-50 text-center items-center text-8xl projectLi '> <p className='ml-16'>Raj </p></div> 
-        </li> */}
-        <li className="ul">
-          <div className="li_inside absolute">
-            <h1 className=" text-green-700 w-96 h1_raj ">Awesome Text</h1>
-          </div>
-        </li>
-   
-      </ul>
+      <div className="bg-black text-white relative ">
 
-      <ul className="li_container">
-        {/* <li className='li_1 '>
-        <div className=' flex justify-start  align-middle h-36 bg-slate-50 text-center items-center text-8xl projectLi '> <p className='ml-16'>Raj </p></div> 
-        </li> */}
-        <li className="ul">
-          <div className="li_inside absolute">
-            <h1 className=" text-green-700 w-96 h1_raj ">Awesome Text</h1>
-          </div>
-        </li>
-   
-      </ul>
-
+      <div className="img-wrapper">
+      <div className="img-placeholder"> <img src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" /> </div>
     </div>
+    <h1 className=" text-center text-8xl uppercase p-10">Projetcs</h1>
+        <ul className="li_container">
+          {/* <li className="custom-li">
+            <div className="li_inside">
+ 
+              <a className="img-hover-effect-link  rajjj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">Kitty</a>
+            </div>
+          </li>
+          <li className="custom-li">
+            <div className="li_inside">
+         
+              <a className="img-hover-effect-link rajjj h1_raj"  data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">Awesome Text</a>
+            </div>
+          </li>
 
 
+       */}
+          {/* <li className="custom-li">
+            <Link href="/">
+          <div className="li_inside">
+          <div className="img-hover-effect-link li_inside h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">PS</h1>
+        
+        
+            </div>
+            </div>
+            </Link>
+          </li>
 
+
+          <li className="custom-li">
+            <Link href="/">
+          <div className="li_inside">
+          <a className="img-hover-effect-link li_inside  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">Awesome Text</h1>
+        
+        
+            </a>
+            </div>
+            </Link>
+          </li> */}
+
+<li className="custom-li">
+            <a href="/">
+          <div className="li_inside">
+          <div className="img-hover-effect-link li_inside  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">Slots Paradise</h1>
+        
+        
+            </div>
+            </div>
+            </a>
+          </li>
+
+<li className="custom-li">
+            <a href="/">
+          <div className="li_inside">
+          <div className="img-hover-effect-link li_inside  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">MovieFy</h1>
+        
+        
+            </div>
+            </div>
+            </a>
+          </li>
+
+          <li className="custom-li">
+            <a href="/">
+          <div className="li_inside">
+          <div className="img-hover-effect-link li_inside  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">Vibrary</h1>
+        
+        
+            </div>
+            </div>
+            </a>
+          </li>
+
+          <li className="custom-li">
+          <a href="/">
+          <div className="li_inside">
+          <div className="img-hover-effect-link  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">PointsPread</h1>
+        
+        
+            </div>
+            </div>
+            </a>
+          </li>
+
+          <li className="custom-li">
+            <a href="/">
+          <div className="li_inside">
+          <div className="img-hover-effect-link li_inside  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">Insider Betting Digest</h1>
+        
+        
+            </div>
+            </div>
+            </a>
+          </li>
+          {/* <li className="custom-li">
+            <div className="li_inside">
+              <h1 className="text-green-700 w-96 h1_raj">Awesome Text cbvnv</h1>
+            </div>
+          </li> */}
+          <li className="custom-li">
+            <a href="">
+          <div className="li_inside">
+          <div className="img-hover-effect-link  h1_raj" data-src="https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" href="#">
+ 
+              <h1 className="text-green-700 w-96 rajjj">Betus</h1>
+        
+        
+            </div>
+            </div>
+            </a>
+          </li>
+
+
+          {/* Add more li elements as needed */}
+        </ul>
+      </div>
     </>
-    
   );
 }
 
