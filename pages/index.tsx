08e3bfc9c4a67a6aafee643129656d0e3911526b
rajpatel2435 <rajpatel2435@gmail.com>
2023-11-21 22:40:16 -0500

    fix: Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Home1 from './Home1'
+import ProjetcLi from './components/ProjectLi'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
    <>
    
-   <Home1 />
+
+   <ProjetcLi />
+
    </>
   )
 }
