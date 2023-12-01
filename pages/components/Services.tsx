ea6472ec767f25daee1e8bbd7b7b46b384dbcb3a
rajpatel2435 <rajpatel2435@gmail.com>
2023-11-30 23:02:34 -0500

    feat: services and styles

---
+++ pages/components/Services.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div>
+        <div className='flex justify-center  items-center h-screen bg-black text-white text-8xl p-10 flex-col'>
+            
+  
+            <p className='p-5'>Web, UX/UI Designer</p>
+            <p className='p-5'>Web Developer</p>
+            <p className='p-5'>Speed Optimization</p>
+            <p className='p-5'>SEO</p>
+            <p></p>
+    
+    </div>
+    </div>
+  )
+}
+
+export default Services
